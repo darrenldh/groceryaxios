71aa78da523357308b70c0a6fbd16afe9b7860c8
darrenldh <51434577+darrenldh@users.noreply.github.com>
2020-03-28 02:08:18 +0800

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
+    apiKey: "AIzaSyA81TEXDfxDeM5ORmvpO87sPckfGjPZiHQ",
+    authDomain: "axios-2a81c.firebaseapp.com",
+    databaseURL: "https://axios-2a81c.firebaseio.com",
+    projectId: "axios-2a81c"
 
   };
   
